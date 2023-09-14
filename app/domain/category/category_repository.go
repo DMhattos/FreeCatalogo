cc7ff569b6b4cc866a111f6556501bd38a2d8f8d
D Mhattos <105811269+DMhattos@users.noreply.github.com>
2023-09-14 15:12:16 -0300

    adicionado context para lidar com as operações do db

--- app/domain/category/category_repository.go
+++ app/domain/category/category_repository.go
@@ -1,17 +1,19 @@
 package category
 
+import "context"
+
 // CategoryRepository define métodos para acessar e persistir dados de categorias.
 type CategoryRepository interface {
 	// CreateCategory cria uma nova categoria e retorna a categoria criada ou um erro.
-	CreateCategory(category *Category) (*Category, error)
+	CreateCategory(ctx context.Context, category *Category) (*Category, error)
 	// ListCategories lista todas as categorias disponíveis e retorna uma lista de categorias ou um erro.
-	ListCategories() ([]*Category, error)
+	ListCategories(ctx context.Context) ([]*Category, error)
 	// GetCategoryByID recupera uma categoria pelo seu ID e retorna a categoria ou um erro se a categoria não for encontrada.
-	GetCategoryByID(id int) (*Category, error)
+	GetCategoryByID(ctx context.Context, id int) (*Category, error)
 	// GetCategoryByName recupera uma categoria pelo seu name e retorna a categoria ou um erro se a categoria não for encontrada.
-	GetCategoryByName(name string) (*Category, error)
+	GetCategoryByName(ctx context.Context, name string) (*Category, error)
 	// UpdateCategory atualiza uma categoria existente e retorna a categoria atualizada ou um erro.
-	UpdateCategory(category *Category) (*Category, error)
+	UpdateCategory(ctx context.Context, category *Category) (*Category, error)
 	// DeleteCategory exclui uma categoria pelo seu ID e retorna um erro se a exclusão falhar.
-	DeleteCategory(id int) error
+	DeleteCategory(ctx context.Context, id int) error
 }
--- app/infra/database/category_repository.go
+++ app/infra/database/category_repository.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 
 	model "github.com/DMhattos/FreeCatalogo/app/domain/category"
@@ -15,31 +16,31 @@ func NewCategoryRepository(db *sql.DB) *CategoryRepository {
 }
 
 // InsertCategory insere uma nova categoria no banco de dados.
-func (repo *CategoryRepository) InsertCategory(category *model.Category) error {
+func (repo *CategoryRepository) InsertCategory(ctx context.Context, category *model.Category) error {
 	// Implemente a lógica de inserção aqui.
 	return nil
 }
 
 // GetCategoryByID obtém uma categoria pelo seu ID no banco de dados.
-func (repo *CategoryRepository) GetCategoryByID(id int) (*model.Category, error) {
+func (repo *CategoryRepository) GetCategoryByID(ctx context.Context, id int) (*model.Category, error) {
 	// Implemente a lógica de consulta aqui.
 	return nil, nil
 }
 
 // GetCategoryByName obtém uma categoria pelo seu name no banco de dados.
-func (repo *CategoryRepository) GetCategoryByName(name string) (*model.Category, error) {
+func (repo *CategoryRepository) GetCategoryByName(ctx context.Context, name string) (*model.Category, error) {
 	// Implemente a lógica de consulta aqui.
 	return nil, nil
 }
 
 // UpdateCategory atualiza uma categoria no banco de dados.
-func (repo *CategoryRepository) UpdateCategory(category *model.Category) error {
+func (repo *CategoryRepository) UpdateCategory(ctx context.Context, category *model.Category) error {
 	// Implemente a lógica de atualização aqui.
 	return nil
 }
 
 // DeleteCategory exclui uma categoria do banco de dados.
-func (repo *CategoryRepository) DeleteCategory(id int) error {
+func (repo *CategoryRepository) DeleteCategory(ctx context.Context, id int) error {
 	// Implemente a lógica de exclusão aqui.
 	return nil
 }

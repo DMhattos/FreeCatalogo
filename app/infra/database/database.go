92a6e3ff45594f156a3e2e182ae137a6e8a2ca04
D Mhattos <105811269+DMhattos@users.noreply.github.com>
2023-09-14 14:38:38 -0300

    configuração do db e erros personalizados

--- app/infra/database/database.go
+++ app/infra/database/database.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/DMhattos/FreeCatalogo/config"
+	_ "github.com/lib/pq"
+)
+
+// OpenConnection abre uma conexão com o banco de dados PostgreSQL.
+func OpenConnection() (*sql.DB, error) {
+	// Obtenha as configurações do banco de dados do arquivo de configuração.
+	dbConfig := config.GetConfig().Database
+
+	// Valide as configurações.
+	if dbConfig.Host == "" || dbConfig.Port == 0 || dbConfig.User == "" ||
+		dbConfig.Password == "" || dbConfig.DbName == "" {
+		return nil, fmt.Errorf("configurações de banco de dados incompletas")
+	}
+
+	// Crie a string de conexão.
+	connectionString := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.DbName)
+
+	// Abra a conexão com o banco de dados.
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao abrir a conexão com o banco de dados: %v", err)
+	}
+
+	// Verifique se a conexão é válida.
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("falha ao verificar a conexão com o banco de dados: %v", err)
+	}
+
+	return db, nil
+}
+
+// CloseConnection fecha a conexão com o banco de dados.
+func CloseConnection(db *sql.DB) {
+	if db != nil {
+		db.Close()
+	}
+}
--- app/utils/error.go
+++ app/utils/error.go
@@ -3,7 +3,17 @@ package utils
 import "errors"
 
 var (
-	//Config Erros
+	// Config Errors
 	ErrReadFileConfig   = errors.New("erro ao ler o arquivo de configuração")
 	ErrDecodeFileConfig = errors.New("erro ao decodificar o arquivo de configuração")
+
+	// Database Errors
+	ErrNotOpenConn        = errors.New("erro em conectar ao banco de dados")
+	ErrQueryFailed        = errors.New("erro na consulta ao banco de dados")
+	ErrInsertFailed       = errors.New("erro ao inserir dados no banco de dados")
+	ErrUpdateFailed       = errors.New("erro ao atualizar dados no banco de dados")
+	ErrDeleteFailed       = errors.New("erro ao excluir dados no banco de dados")
+	ErrTransactionFailed  = errors.New("erro ao executar transação no banco de dados")
+	ErrRecordNotFound     = errors.New("registro não encontrado no banco de dados")
+	ErrDatabaseConnection = errors.New("erro na conexão com o banco de dados")
 )
